73b2892c37b1d19dc789556c64268cc7ca07ce8c
Julia Retuerta <julia.retuertanogales@alumnostrazos.net>
2024-05-08 14:59:12 +0200

    clase-8-mayo

--- 24-05-07-formulario-js/js/formulario-js.js
+++ 24-05-07-formulario-js/js/formulario-js.js
@@ -18,36 +18,90 @@ function enviar() {
   asunto.classList.remove('rojo');
   servicios.classList.remove('rojo');
   mensaje.classList.remove('rojo');
+  document.querySelector('#nombre + p').innerHTML = '';
+  document.querySelector('#apellido + p').innerHTML = '';
+  document.querySelector('#email + p').innerHTML = '';
+  document.querySelector('#asunto + p').innerHTML = '';
+  document.querySelector('#servicios + p').innerHTML = '';
+  document.querySelector('#mensaje + p').innerHTML = '';
+  document.querySelector('#politica ~ p').innerHTML = '';
+
+  let cNombre = true;
+  let cApellido = true;
+  let cEmail = true;
+  let cAsunto = true;
+  let cServicios = true;
+  let cMensaje = true;
+  let cPolitica = true;
+
+  let patron =
+    /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
   if (nombre.value == '') {
-    respuesta.innerHTML = 'Debe completar el campo Nombre';
+    document.querySelector('#nombre + p').innerHTML =
+      'Debe completar el campo Nombre';
     nombre.classList.add('rojo');
+    cNombre = false;
   }
   if (apellido.value == '') {
-    respuesta.innerHTML = 'Debe completar el campo Apellido';
+    document.querySelector('#apellido + p').innerHTML =
+      'Debe completar el campo Apellido';
     apellido.classList.add('rojo');
+    cApellido = false;
   }
   if (email.value == '') {
-    respuesta.innerHTML = 'Debe completar el campo Email';
+    document.querySelector('#email + p').innerHTML =
+      'Debe completar el campo Email';
     email.classList.add('rojo');
+    cEmail = false;
   }
+
+  if (email.value != '') {
+    if (patron.test(email.value)) {
+    } else {
+      document.querySelector('#email + p').innerHTML = 'El email no es válido';
+      email.classList.add('rojo');
+      cEmail = false;
+    }
+  }
+
   if (asunto.value == '') {
-    respuesta.innerHTML = 'Debe completar el campo Asunto';
+    document.querySelector('#asunto + p').innerHTML =
+      'Debe completar el campo Asunto';
     asunto.classList.add('rojo');
+    cAsunto = false;
   }
   if (servicios.value == '') {
-    respuesta.innerHTML = 'Debe completar el campo Servicios';
+    document.querySelector('#servicios + p').innerHTML =
+      'Debe completar el campo Servicios';
     servicios.classList.add('rojo');
+    cServicios = false;
   }
   if (mensaje.value == '') {
-    respuesta.innerHTML = 'Debe completar el campo Mensaje';
+    document.querySelector('#mensaje + p').innerHTML =
+      'Debe completar el campo Mensaje';
     mensaje.classList.add('rojo');
+    cMensaje = false;
   }
   /*else if (!politica.checked) {
-  }*/ if (politica.checked == false) {
-    respuesta.innerHTML = 'Debe aceptar la Política de privacidad';
+  }*/
+
+  if (politica.checked == false) {
+    document.querySelector('#politica ~ p').innerHTML =
+      'Debe aceptar la Política de privacidad';
+    cPolitica = false;
   } else {
-    respuesta.innerHTML = 'Formulario enviado';
-    document.querySelector('#formulario').submit();
+    if (
+      cNombre &&
+      cApellido &&
+      cEmail &&
+      cAsunto &&
+      cServicios &&
+      cMensaje &&
+      cPolitica
+    ) {
+      respuesta.innerHTML = 'Formulario enviado';
+      document.querySelector('#formulario').submit();
+    }
   }
 }
