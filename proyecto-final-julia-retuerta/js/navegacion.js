00d09804c1b7143fb5184f8f6feb288a296d5cd0
juliaret <julia.retuertanogales@alumnostrazos.net>
2024-05-21 00:13:00 +0200

    practica-casa-20-mayo

--- proyecto-final-julia-retuerta/js/navegacion.js
+++ proyecto-final-julia-retuerta/js/navegacion.js
@@ -1,3 +1,5 @@
+//MENÚ HAMBURGUESA MÓVIL
+
 if (window.innerWidth < 768) {
   document.querySelector(".nav__menu").classList.add("nav__menu--ocultar");
   document.querySelector("#hamburguesa").addEventListener("click", function () {
@@ -20,3 +22,30 @@ if (window.innerWidth < 768) {
     }
   });
 }
+
+//SUBMENÚ
+
+document
+  .querySelector(".nav__subnavbar")
+  .classList.add("nav__subnavbar--ocultar");
+document.querySelector("#desplegable").addEventListener("click", function () {
+  //console.log('submenu...');
+
+  document
+    .querySelector(".nav__subnavbar")
+    .classList.toggle("nav__subnavbar--ocultar");
+
+  if (
+    document
+      .querySelector(".nav__subnavbar")
+      .classList.contains("nav__subnavbar--ocultar")
+  ) {
+    document
+      .querySelector("#desplegable i")
+      .classList.replace("bi-caret-up-fill", "bi-caret-down-fill");
+  } else {
+    document
+      .querySelector("#desplegable i")
+      .classList.replace("bi-caret-down-fill", "bi-caret-up-fill");
+  }
+});
